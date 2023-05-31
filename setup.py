c58a5a372cd247ebe10afb76ab7f83012e0c3fe8
DaG <christos.daglaroglou@gmail.com>
2023-05-31 13:24:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 class Colors:
     dred = "\033[31m"
@@ -17,3 +18,19 @@ class Colors:
     cyan = "\033[96m"
     white = "\033[97m"
     reset = "\033[0m"
+
+print(f'''
+{Colors.yellow}  _                                  {Colors.dblue} _____  _____   _____ {Colors.reset}
+{Colors.yellow} | |                                 {Colors.dblue}|  __ \|  __ \ / ____|{Colors.reset}
+{Colors.yellow} | |     ___  __ _  __ _ _   _  ___  {Colors.dblue}| |__) | |__) | |     {Colors.reset}
+{Colors.yellow} | |    / _ \/ _` |/ _` | | | |/ _ \ {Colors.dblue}|  _  /|  ___/| |     {Colors.reset}
+{Colors.yellow} | |___|  __/ (_| | (_| | |_| |  __/ {Colors.dblue}| | \ \| |    | |____ {Colors.reset}
+{Colors.yellow} |______\___|\__,_|\__, |\__,_|\___| {Colors.dblue}|_|  \_\_|     \_____|{Colors.reset}
+{Colors.yellow}                    __/ |                                                {Colors.reset}
+{Colors.yellow}                   |___/                                                 {Colors.reset}
+''')
+time.sleep(1.5)
+
+print(f'{Colors.dmagenta}Installing dependencies...{Colors.reset}')
+
+os.system('pip3 install pypresence requests psutil')
